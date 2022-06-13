2e5d9f60ebb366712518507f9b81539069f82166
Volker Dufner <dufnervolker@gmail.com>
2022-06-13 19:57:42 +0200

    Update Backend Task

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -12,41 +12,96 @@
  **********************************************************************************
  */
 
-import Web3 from 'web3';
-import ContractKit from "@celo/contractkit";
+import Web3 from "web3";
+import { AbiItem } from "web3-utils";
+import path from "path";
+import { readFileSync } from "fs";
+import { ContractKit, newKit } from "@celo/contractkit";
+import deployedContracts from "@ipanema/hardhat/deployments/hardhat_contracts.json";
+import { CentralizedLoan } from "@ipanema/hardhat/types/CentralizedLoan";
 
-const deployedContracts: ContractJSON = import("@ipanema/hardhat/deployments/hardhat_contracts.json");
-const web3 = new Web3("https://alfajores-forno.celo-testnet.org");
-const kit = ContractKit.newKitFromWeb3(web3);
+// interface ContractJSON {
+//   [key: string]: any;
+// }
 
-const getAccount = require("./getAccount").getAccount;
+// function loanRequest(addressUser: string, numOfDays: number, amount: number) {
+//   grantLoan(addressUser, calculateConditions(addressUser, numOfDays, amount));
+// }
 
-function loanRequest(addressUser: string, numOfDays: number, amount: number) {
-    grantLoan(addressUser, calculateConditions(addressUser, numOfDays, amount));
-}
+// function calculateConditions(
+//   addressUser: string,
+//   numOfDays: number,
+//   amount: number
+// ): number {
+//   let interestRate = 500; // Calculated by the banks risk algorithm
+//   return interestRate;
+// }
 
-function calculateConditions(addressUser: string, numOfDays: number, amount: number): number {
-    let interestRate = 500;  // Calculated by the banks risk algorithm
-    return interestRate;
-}
+const main = async () => {
+  const pathToPrivateKey = path.join(__dirname, "./.secret");
+  const privateKey =
+    "959a7b54e849bafce5b40b1c6645d1b8b8b4b7557f1b9a8d8de7630d7a85cbb2";
+  const address = "0x6c87D48da426993ca8Eb3e8cD432CCCBD61c8cDc";
+  const kit: ContractKit = newKit("https://alfajores-forno.celo-testnet.org");
 
+  // default from account
+  kit.defaultAccount = address;
 
+  // add the account private key for tx signing when connecting to a remote node
+  kit.connection.addAccount(privateKey);
 
-const contract = new kit.web3.eth.Contract(
-    contracts?.CentralizedLoan.abi,
+  const contracts = deployedContracts["44787"][0].contracts;
+  const oneGold = kit.connection.web3.utils.toWei("1", "ether");
+
+  const contract = new kit.connection.web3.eth.Contract(
+    contracts?.CentralizedLoan.abi as AbiItem[] | AbiItem,
     contracts?.CentralizedLoan.address
-);
+  ) as any as CentralizedLoan;
+
+  let estimateGas = await contract.methods
+    .offerLoan(
+      100,
+      5,
+      1655116468,
+      "0x8F8aB9A6374D1a971ca614bC277218929869Bf89",
+      "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
+      //   loanAmount,
+      //   interestAmount,
+      //   repayByTimestamp,
+      //   borrower,
+      //   ercAddress
+    ).estimateGas();
+
+    console.log(estimateGas);
 
 
-async function grantLoan(addressUser: string, interestRate: number) {
-    let account = await getAccount();
-    kit.connection.addAccount(account.privateKey);
-    let amount = 10000;
+    await contract.methods
+    .offerLoan(
+      100,
+      5,
+      1655116468,
+      "0x8F8aB9A6374D1a971ca614bC277218929869Bf89",
+      "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
+      //   loanAmount,
+      //   interestAmount,
+      //   repayByTimestamp,
+      //   borrower,
+      //   ercAddress
+    ).send({ from: address });
 
-    let multiLoan = await kit.contracts.
-    let cEURtx = await multiLoan.offerLoan(loanAmount, interestAmount, repayByTimestamp, borrower, ercAddress).send({from: account.address});
-}
+    console.log("Loan offerted");
+};
 
+main().catch(err => {
+  console.error(err);
+});
+
+// };
+
+// main().catch(err => {
+//   console.error(err);
+// });
 // Alfajores Testnet Account:
+// cUSD: 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1
 // 0x6c87D48da426993ca8Eb3e8cD432CCCBD61c8cDc
 // 959a7b54e849bafce5b40b1c6645d1b8b8b4b7557f1b9a8d8de7630d7a85cbb2

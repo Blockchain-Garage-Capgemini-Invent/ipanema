55c2742a4b65e1d639fa00fdd1200a7e04bf5154
Volker Dufner <dufnervolker@gmail.com>
2022-06-15 18:49:23 +0200

    Fix Header Style

--- packages/app/src/components/Header.tsx
+++ packages/app/src/components/Header.tsx
@@ -42,46 +42,20 @@ export default function Header() {
   const { theme: themeContext, setTheme } = useThemeContext();
 
   return (
-    <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" style={{ background: "transparent", boxShadow: "none" }}>
-          <Toolbar sx={{ gap: { md: 2, xs: 0.5 } }}>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Ipanema Finance
-            </Typography>
-            <AccountDetails />
-            <ThemeSwitcher
-              sx={{ m: 1 }}
-              onChange={e => setTheme(e.target.checked)}
-              checked={themeContext}
-            />
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </>
-
-    // <div>
-    //     <nav class="Navigation">
-    //         <ul>
-    //             {/* --- --- --- --- --- Header + Space --- --- --- --- --- */}
-    //             <li><div class="HeaderTitle">Ipanema</div></li>
-
-    //             {/* --- --- --- --- --- Platform Select + Space --- --- --- --- --- */}
-    //             <li><button class="PlatformButton">
-    //                 <ul>
-    //                     <li><img alt="ETH" src={require('./images/ethereumlogo.png')} width="25" height="25" class="PlatformButtonImage"/></li>
-    //                     <li><div class="PlatformButtonText">Ethereum Mainnet</div></li>
-    //                     <li><svg height="20" width="20"><polyline points="6 9 12 15 18 9"></polyline></svg></li>
-    //                 </ul>
-    //                 </button>
-    //             </li>
-
-    //             {/* --- --- --- --- --- Wallet Select + Space --- --- --- --- --- */}
-    //             <li>{address ? (<button class="WalletButton">Connected to {address}</button>) :
-    //             (<button onClick={connect} class="WalletButton">Connect wallet</button>)}</li>
-    //         </ul>
-    //     </nav>
-    //     <hr class="HorizontalLine"/>
-    // </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{ gap: { md: 2, xs: 0.5 } }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Ipanema Finance
+          </Typography>
+          <AccountDetails />
+          <ThemeSwitcher
+            sx={{ m: 1 }}
+            onChange={e => setTheme(e.target.checked)}
+            checked={themeContext}
+          />
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }

74402cc04fd38a828613db69e19755f31ddfef15
0xtim <tim.schmitz@capgemini.com>
2022-06-15 11:27:55 +0200

    Include ConnectWallet

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,10 +1,12 @@
 import { CustomThemeProvider } from "./contexts/userTheme";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { SnackbarProvider } from 'notistack';
 import { CeloProvider } from "@celo/react-celo";
 import "@celo/react-celo/lib/styles.css";
 import Header from "./components/Header";
 // import LoanBox from "./components/LoanBox";
-import MaintenanceBox from "./components/MaintenanceBox";
+import ConnectWallet from "./components/ConnectWallet";
+// import MaintenanceBox from "./components/MaintenanceBox";
 import Footer from "./components/Footer";
 
 /*
@@ -20,23 +22,26 @@ function CheckAccount() {
 export default function App() {
   return (
     <CustomThemeProvider>
-      <CeloProvider
-        dapp={{
-          name: "Ipanema DApp",
-          description: "A demo DApp to showcase functionality",
-          url: "",
-          icon: "",
-        }}
-      >
-        <Router>
-          <Header />
-          <Routes>
-            {/*<Route path="/" element={<LoanBox />} />*/}
-            <Route path="/" element={<MaintenanceBox />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </CeloProvider>
+        <SnackbarProvider>
+            <CeloProvider
+                dapp={{
+                    name: "Ipanema DApp",
+                    description: "A demo DApp to showcase functionality",
+                    url: "",
+                    icon: "",
+                }}
+            >
+                <Router>
+                    <Header />
+                    <Routes>
+                        {/*<Route path="/" element={<LoanBox />} />*/}
+                        {/*<Route path="/" element={<MaintenanceBox />} />*/}
+                        <Route path="/" element={<ConnectWallet />} />
+                    </Routes>
+                    <Footer />
+                </Router>
+            </CeloProvider>
+        </SnackbarProvider>
     </CustomThemeProvider>
   );
 }

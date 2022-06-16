e85812e97de5aea397d69cba158bceac87032921
0xtim <tim.schmitz@capgemini.com>
2022-06-16 12:08:20 +0200

    Info Box added

--- packages/app/src/components/ConnectWallet.tsx
+++ packages/app/src/components/ConnectWallet.tsx
@@ -30,6 +30,19 @@ export default function ConnectWallet() {
             </FormControl>
           </CardContent>
         </Card>
+
+        <Card sx={{ mt: 5 }}>
+          <CardContent>
+            <Typography variant="h5">How it works:</Typography>
+            <Typography variant="h6" marginTop={2}>1. Authenticate</Typography>
+            <Typography>-Enter your wallet address at your banking app</Typography>
+            <Typography>-Your bank will verify your address</Typography>
+            <Typography variant="h6" marginTop={2}>2. Get a loan</Typography>
+            <Typography>-Connect your wallet in the frontend</Typography>
+            <Typography>-Enter time range and amount</Typography>
+            <Typography>-Get the loan from your bank</Typography>
+          </CardContent>
+        </Card>
       </Grid>
     </Grid>
   );

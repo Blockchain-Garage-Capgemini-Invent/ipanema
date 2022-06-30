d8fe5c890fbdd43d7e0327e66dd3a80fd1da8d1e
Volker Dufner <dufnervolker@gmail.com>
2022-06-30 19:22:28 +0200

    Styling Improvements

--- packages/app/src/components/Header.tsx
+++ packages/app/src/components/Header.tsx
@@ -49,7 +49,7 @@ export default function Header() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" color="default">
         <Toolbar sx={{ gap: { md: 2, xs: 0.5 } }}>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             <Link href="/" color="inherit" underline="none">
